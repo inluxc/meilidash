9de23ff8ee438bb58076984f09d1c7ff2c84be4e
fcarvalhido <fulvio@hatchstudio.co>
2022-06-13 17:02:34 +0100

    Homepage Update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Homepage</div>
+  <h1>MeiliSeach Dashboard</h1>
+  <span
+    >MeiliSeach Dashboard is a simple Dahsboard and Admin for
+    <a href="https://www.meilisearch.com/" target="_blank"
+      >MeiliSeach Search Service</a
+    >.</span
+  >
 </template>
 <script lang="ts">
 export default {
